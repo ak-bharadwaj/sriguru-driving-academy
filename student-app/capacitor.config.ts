b04ec8bb4aff3ee63c0afd392415e02731938f11
AKSHITH BHARADWAJ D L T <ak-bharadwaj@users.noreply.github.com>
2026-07-16 20:01:35 +0530

    add-server-client-id-to-google-signin-config

--- student-app/capacitor.config.ts
+++ student-app/capacitor.config.ts
@@ -12,6 +12,7 @@ const config: CapacitorConfig = {
   plugins: {
     GoogleSignIn: {
       clientId: '1064521036931-ako2v5m58nbc0hbra9p5a5h7an1tm4be.apps.googleusercontent.com',
+      serverClientId: '1064521036931-ako2v5m58nbc0hbra9p5a5h7an1tm4be.apps.googleusercontent.com',
     },
   },
 };
